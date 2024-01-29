8dcdd30847e58384c4729f07b043f97d8361a070
danny gonzalez <jesusgalicia2019@gmail.com>
2024-01-29 15:43:01 -0400

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from '@material-tailwind/react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
